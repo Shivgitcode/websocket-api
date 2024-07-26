38d29d1937fd78351a0bede31761c7c48fe59ece
shivansh <shivneeraj2004@gmail.com>
2024-07-26 23:04:41 +0530

    first commit

---
+++ dist/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { WebSocketServer, WebSocket } from "ws";
+const app = express();
+const server = app.listen(8000);
+const wss = new WebSocketServer({ server });
+wss.on('connection', (socket) => {
+    socket.on('message', (data, isBinary) => {
+        console.log('received data: %s', data);
+        wss.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(data, { binary: isBinary });
+            }
+        });
+    });
+});
---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import express from "express"
+import { WebSocketServer, WebSocket } from "ws"
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("hello")
+})
+
+
+const server = app.listen(8000)
+
+const wss = new WebSocketServer({ server })
+
+wss.on('connection', (socket) => {
+    socket.on('message', (data, isBinary) => {
+        console.log('received data: %s', data)
+        wss.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(data, { binary: isBinary })
+            }
+        })
+
+    })
+
+})
+
